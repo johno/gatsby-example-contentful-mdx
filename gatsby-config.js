bd3810b6db59d2cc69c7e3991b6f20651c4d9e59
John Otander <johnotander@gmail.com>
2019-07-24 09:50:13 -0600

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,8 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST
       }
     }
   ]
